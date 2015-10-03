f4c29af8c83e4311f49476edd30b99fd0f2a56d9
Tacit <rbenlizar@gmail.com>
2015-10-03 01:15:35 +0000

    added config for sending emails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,6 +26,20 @@ Rails.application.configure do
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
+  config.action_mailer.smtp_settings ={
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: ENV['user_Domain'],
+    authentication: 'plain',
+    enable_starttls_auto: true,
+    user_name: ENV['gmail_username'],
+    password: ENV['gmail_password']   
+    }
+  #ActionMail Config
+  config.action_mailer.default_url_options = {:host => 'localhost:3000'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
+  config.action_mailer.raise_delivery_errors = true
 
   # Asset digests allow you to set far-future HTTP expiration dates on all assets,
   # yet still be able to expire them through the digest params.
